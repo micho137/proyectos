936ec2cf7381df90c95d4a81bd8905bf15540126
micho137 <michaenrangelmr@gmail.com>
2021-09-07 17:07:11 -0500

    terminado

--- Frecuencia/src/frecuencia/Frecuencia.java
+++ Frecuencia/src/frecuencia/Frecuencia.java
@@ -20,6 +20,7 @@ public class Frecuencia {
         Frecuencia fre = new Frecuencia();
         int numero;
         int n = 0;
+        double x1 = 2.5, x2 = 8.5, x3 = 13.5, x4 = 18;
         while (n < 15) {
 
             String str = JOptionPane.showInputDialog(null, "Ingresa un numero de indice " + (n + 1) + " en el rango de [0,20]");
@@ -41,10 +42,15 @@ public class Frecuencia {
                 fre.invalido++;
             }
         }
-        String moda = fre.in1 + ", " + fre.in2 + ", " + fre.in3 + ", " + fre.in4 + "";
-        System.out.println(moda);
-        JOptionPane.showMessageDialog(null, "Cantidades invalidas: " + fre.invalido);
-        String porcentajes = fre.porcentaje(fre.in1) + ", " + fre.porcentaje(fre.in2) + ", " + fre.porcentaje(fre.in3) + ", " + fre.porcentaje(fre.in4);
+        int Y = fre.in1+fre.in2+fre.in3+fre.in4;
+        double X = (x1*fre.in1)+(x2*fre.in2)+(x3*fre.in3)+(x4*fre.in4);
+        double Z = X/Y;
+        String moda = "Cantidad de datos Ingresados por rangos\n" + "[0,5]: " + fre.in1 + "\n" + "[6,11]: " + fre.in2 + "\n" + "[12,15]: " + fre.in3 + "\n" + "[16,20]: " + fre.in4 + "\n";
+        JOptionPane.showMessageDialog(null, moda);
+        String porcentajes = "Porcentaje de datos Ingresados por rangos\n" + "[0,5]: " + fre.porcentaje(fre.in1) + "%" + "\n" + "[6,11]: " + fre.porcentaje(fre.in2) + "%" + "\n" + "[12,15]: " + fre.porcentaje(fre.in3) + "%" + "\n" + "[16,20]: " + fre.porcentaje(fre.in4) + "%" + "\n";
         JOptionPane.showMessageDialog(null, "Porcentajes: " + porcentajes);
+        JOptionPane.showMessageDialog(null, "Media: "+Z);
+        JOptionPane.showMessageDialog(null, "Cantidades invalidas: " + fre.invalido);
+
     }
 }
